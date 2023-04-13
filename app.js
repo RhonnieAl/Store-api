a7944e0b79d4fce046f34d3875a7aadbc44ad770
Rhonnie Al <rhonnieallan@protonmail.com>
2023-04-13 13:38:42 +0300

    Setup Server Boilerplate

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('04 Store API')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
+
+/* Middleware */
+// Handle JSON data using Express middleware
+app.use(express.json());
+
+//Routes
+
+// Test route for Homepage (To be changed later)
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .send(
+      "<h2>Hello this is the Home Page</h2><a href='/api/v1/products'>Click here to see all Products</a>"
+    );
+});
+
+// Set up error handlers middlware
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+// Set up server port to be set by VPS or default to 3000
+const port = process.env.PORT || 3000;
+
+// Start Server
+app.listen(port, () => {
+  console.log(`Server is Listening on Port ${port}...`);
+});
--- middleware/not-found.js
+++ middleware/not-found.js
@@ -1,3 +1,4 @@
-const notFound = (req, res) => res.status(404).send('Route does not exist')
+// If the route does not exist, send a 404 status code and the following message
+const notFound = (req, res) => res.status(404).send("Route does not exist");
 
-module.exports = notFound
+module.exports = notFound;
