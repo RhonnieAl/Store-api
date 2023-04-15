21b8c1f5054403f58a01f748a5e6a85847708afe
Rhonnie Al <rhonnieallan@protonmail.com>
2023-04-15 20:25:59 +0300

    Setup search functionality

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,15 @@
-const products = require("../products.json");
+// Import Mongoose Model
+const Product = require("../models/product");
 
-const getAllProducts = async (req, res) => {
+// This is a Test endpoint
+const getAllProductsStatic = async (req, res) => {
+  const products = await Product.find({ name: "wooden desk" });
   res.status(200).json({ products });
 };
 
-const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ msg: "Success! This is a Test Route" });
+const getAllProducts = async (req, res) => {
+  const products = await Product.find(req.query);
+  res.status(200).json({ products, numHits: products.length });
 };
 
 module.exports = { getAllProducts, getAllProductsStatic };
